ac229dcf747b1ab5aad26d5be7f6121f47e76cf5
Jamie Brown <jmbrowncow@gmail.com>
2018-09-02 20:23:00 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to '/welcome'
     else
-      redirect_to 'users/new'
+      redirect_to '/users/new'
     end
   end
 
