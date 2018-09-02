e3052dc714630c147188d3957cab0221e87a0517
Jamie Brown <jmbrowncow@gmail.com>
2018-09-02 19:58:40 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,13 @@
+<<<<<<< HEAD
 class SessionsController < ApplicationController
+=======
+class SessionsController <ApplicationController
+
+  def new
+  end
+
+  def created
+  end
+
+>>>>>>> 9e5cd8104009335dc01abffd1730b48726160939
 end
